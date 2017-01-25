ac201a1d51d1925271aa8f435a9ab3bb167564c5
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-01-25 20:01:46 +0300

    lab1 new version

---
+++ Documents/Visual Studio 2015/Projects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+
+    {
+        static bool isEven(int a)
+        {
+            if (a % 2 == 0)
+                return true;
+            return false;
+        }
+            static void Main(string[] args)
+        {
+            int n = args.Length;
+            for (int i = 0; i < n; i++)
+            {
+                string s = args[i]; // "5"
+                int p = int.Parse(s); // p = 5
+
+                if (isEven(p) == false)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
